d57070647cac15b054778501d7ac5d17ebbe0a10
Mohataseem Khan <mohataseem89@gmail.com>
2025-02-16 12:48:02 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,18 +18,8 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
     currFolder = folder;
-    // let a = await fetch(`/${folder}/`)
-    let a = await fetch(`${folder}/`)
+    let a = await fetch(`/${folder}/`)
     // let a = await fetch(`http://192.168.1.108:3000/${folder}/`)
-    // let a = await fetch(`http://192.168.1.107:3000/${folder}/`)
-    // let a = await fetch(`http://127.0.0.1:5500/${folder}/`)
-    // let a = await fetch(`http://192.168.1.105:3002/${folder}/`)
-    // let a = await fetch(`http://192.168.1.105:3000/${folder}/`)
-    // let a = await fetch(`http://192.168.1.104:3000/${folder}/`)
-    // let a = await fetch(`http://127.0.0.1:5500/${folder}/`)
-    // let a = await fetch("http://127.0.0.1:5500/songs/")
-    // let a = await fetch("http://192.168.1.104:3000/songs/")
-    // let a = await fetch("http://127.0.0.1:5500/songs/")
     let response = await a.text();
     // console.log(response)
     let div = document.createElement("div")
